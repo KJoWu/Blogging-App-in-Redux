65a30acb64b2ab0a9c7bf03f9f8b62384e34b328
Silver Rook <kwu1@ualberta.ca>
2016-02-26 15:32:04 -0700

    created new Post route

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import React from "react";
 import { Route, IndexRoute } from "react-router";
 import PostsIndex from "./components/posts_index";
 import App from "./components/app";
+import PostsNew from './components/posts_new';
 
 
 export default(
 	<Route path="/" component={App} > //whenever the user is at this path "/" show this component {App}
 		<IndexRoute component = {PostsIndex} /> //will show / and this route
+		<Route path="posts/new" component={PostsNew} />
 	</Route>
 );
